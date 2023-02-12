9644a98b9192f1201432e7e3b6ec598104074ad3
Viktoriiahumster <vikysenok08@mail.ru>
2023-02-12 20:35:33 +0800

    Improvement

--- file_sorted.py
+++ file_sorted.py
@@ -1,38 +1,26 @@
+from os import listdir
+from os.path import isfile, join
+
 text_dct = {}
+text_name_list = [f for f in listdir() if f.endswith(".txt") and isfile(join(f))]
 
-for i in range(1, 4):
-    text_name = f'{i}.txt'
-    with open(text_name, 'r', encoding='utf-8') as f:
-        text_dct[text_name] = sum(1 for line in f)
+for el in text_name_list:
+    with open(f'{el}', 'r', encoding='utf-8') as f:
+        text = []
+        k = 0
+        for row in f:
+            k += 1
+            t = row.strip().split('\n')
+            text.extend(t)
+        text.insert(0, k)
+        text_dct[el] = text
 
-max_t = text_dct.get(max(text_dct, key=text_dct.get))
-min_t = text_dct.get(min(text_dct, key=text_dct.get))
-max_file = f'{list(text_dct.keys())[list(text_dct.values()).index(max_t)]}'
-min_file = f'{list(text_dct.keys())[list(text_dct.values()).index(min_t)]}'
-second_t = ''
+sorted_dict = dict(sorted(text_dct.items(), key=lambda item: item[1]))
+print(sorted_dict)
 
+with open(f'end_file.txt', 'w', encoding='utf-8') as f:
+    for key in sorted_dict.keys():
+        f.write(f'{key}\n')
+        for el in sorted_dict[key]:
+            f.write(f'{el}\n')
 
-with open('end_file.txt', 'w', encoding='utf-8') as f:
-    f.write(f'{max_file}\n')
-    f.write(f'{max_t}\n')
-with open(f'{max_file}', 'r', encoding='utf-8') as f_f, open('end_file.txt', 'a', encoding='utf-8') as f:
-    for line in f_f:
-        f.write(line)
-    f.write('\n')
-with open('end_file.txt', 'a', encoding='utf-8') as f:
-    for value in text_dct.values():
-        if value < max_t and value > min_t:
-            second_t = f'{list(text_dct.keys())[list(text_dct.values()).index(value)]}'
-            f.write(f'{second_t}\n')
-            f.write(f'{value}\n')
-with open(f'{second_t}', 'r', encoding='utf-8') as f_f, open('end_file.txt', 'a', encoding='utf-8') as f:
-    for line in f_f:
-        f.write(line)
-    f.write('\n')
-with open('end_file.txt', 'a', encoding='utf-8') as f:
-    f.write(f'{min_file}\n')
-    f.write(f'{min_t}\n')
-with open(f'{min_file}', 'r', encoding='utf-8') as f_f, open('end_file.txt', 'a', encoding='utf-8') as f:
-    for line in f_f:
-        f.write(line)
-    f.write('\n')
